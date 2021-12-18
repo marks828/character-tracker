1b0ab2496a5502ec338bae9e67b73a7db94cbdbd
mark <marks828@gmail.com>
2021-12-18 14:59:52 -0500

    tailwindcss working

--- src/characters/Character.js
+++ src/characters/Character.js
@@ -1,3 +1,4 @@
+import '../index.css'
 import { useState } from 'react';
 
 function Character(){
@@ -25,36 +26,43 @@ function Character(){
 
 
     return(
-        <>
-        <h1>Health</h1>
-        <button 
-            onClick={healthUp}
-        >Health Up</button>
-        <p>{health}</p>
-        <button
-            onClick={healthDown}
-        >Health Down</button>
-
-
-        <h1>Experience</h1>
-        <button
-            onClick={experienceUp}
-        >Experience Up</button>
-        <p>{experience}</p>
-        <button
-            onClick={experienceDown}
-        >Experience Down</button>
-
-        <h1>Gold</h1>
-        <button
-            onClick={() => setGold(gold + 1)}
-        >Gold Up</button>
-        <p>{gold}</p>
-        <button
-            onClick={() => setGold(gold - 1)}
-        >Gold Down</button>
+        <div className='sm:grid grid-rows-4 grid-flow-col gap-4'>
+        <div className='py-5 bg-red-500'>
+            <h1 className='sm:grid justify-items-center'>Health</h1>
+            <div className='sm:grid grid-cols-3 gap-2 justify-items-center place-items-center'>
+                <button
+                    onClick={healthDown}
+                >Health Down</button>
+                <p className=''>{health}</p>
+                <button
+                    onClick={healthUp}
+                >Health Up</button>
+            </div>
+        </div>
         
-        </>
+        <div className='py-5 bg-blue-600'>
+            <h1>Experience</h1>
+            <button
+                onClick={experienceDown}
+            >Experience Down</button>
+            <p>{experience}</p>
+            <button
+                onClick={experienceUp}
+            >Experience Up</button>
+        </div>
+        
+        <div className='py-5 bg-amber-300'>
+            <h1>Gold</h1>
+            <button
+                onClick={() => setGold(gold - 1)}
+            >Gold Down</button>
+            <p>{gold}</p>
+            <button
+                onClick={() => setGold(gold + 1)}
+            >Gold Up</button>
+        </div>
+        
+        </div>
     )
 }
 
